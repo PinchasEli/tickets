16e5e4a8d505d2533f8ca7b6875c5634f8271a8f
Pinchas <pinchas28@gmail.com>
2024-01-22 22:03:58 +0200

    CORS

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -11,6 +12,9 @@ migrate = Migrate()
 def create_app(config_mode):
     app = Flask(__name__)
     app.config.from_object(config[config_mode])
+    
+    # Enable CORS for all routes
+    CORS(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
